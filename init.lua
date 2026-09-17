612bdb36e51b6fb1d81b526138232f18a9e0f3e6
Forman37 <forman3@Me.com>
2026-09-17 08:39:59 -0400

    added bootstrap code to install lazy in init

--- init.lua
+++ init.lua
@@ -6,4 +6,19 @@ require("james.keymaps")
 require("james.commands")
 require("james.format")
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup("james.plugins")
